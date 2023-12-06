4be831e9a999b1d04e6068b3b2aa0d327caec98a
Sebastiaan Vercammen <sebastiaan@datameister.nl>
2023-12-06 13:32:46 +0100

    refactored day 1 part 1

---
+++ Day1Part1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Text.RegularExpressions;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Utils;
+
+namespace Day1Part1;
+
+[MinIterationCount(5)]
+[MaxIterationCount(10)]
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Day 1 part 1");
+#if DEBUG
+        new Program().ChallengeLoopVariant(args.FirstOrDefault());
+        new Program().ChallengeRegexVariant(args.FirstOrDefault());
+#else
+        BenchmarkRunner.Run<Program>();
+#endif
+    }
+    
+    private static int CalibrationValueLoopVariant(string input)
+    {
+        var firstDigit = "";
+        var lastDigit = "";
+
+        foreach (var character in input.Where(char.IsNumber))
+        {
+            if (firstDigit == "") firstDigit = character.ToString();
+
+            lastDigit = character.ToString();
+        }
+
+        return int.Parse(firstDigit + lastDigit);
+    }
+
+    public void ChallengeLoopVariant(string? filepath)
+    {
+        var sum = FileReader.Lines(filepath).Sum(CalibrationValueLoopVariant);
+        Console.WriteLine($"Using the loop variant: {sum}");
+    }
+
+    [Benchmark]
+    public void BenchmarkLoopVariant()
+    {
+        ChallengeLoopVariant(null);
+    }
+
+    private readonly Regex _regex = new(@"\d", RegexOptions.Compiled);
+    
+    private int CalibrationValueRegexVariant(string input)
+    {
+        var matches = _regex.Matches(input);
+        return int.Parse(matches[0].Groups[0].Value + matches[^1].Groups[0].Value);
+    }
+
+    public void ChallengeRegexVariant(string? filepath)
+    {
+        var sum = FileReader.Lines(filepath).Sum(CalibrationValueRegexVariant);
+        Console.WriteLine($"Using the regex variant: {sum}");
+    }
+
+    [Benchmark]
+    public void BenchmarkRegexVariant()
+    {
+        ChallengeRegexVariant(null);
+    }
+}
---
+++ Utils/FileReader.cs
@@ -0,0 +1,30 @@
+﻿namespace Utils;
+
+public class FileReader
+{
+    public static IEnumerable<string> Lines(string? path)
+    {
+        string[] paths = path != null
+            ? [path]
+            : [
+                "input.txt",    // dotnet watch
+                "../../../../../../../input.txt",   // benchmark runner
+                "../../input.txt"   // debugger
+            ];
+        
+        var file = paths.FirstOrDefault(File.Exists);
+        if (file == null)
+        {
+            throw new Exception($"Unable to find viable file in [{string.Join(", ", paths)}]");
+        }
+        
+        var sr = new StreamReader(file);
+        var line = sr.ReadLine();
+        while (line != null)
+        {
+            yield return line;
+
+            line = sr.ReadLine();
+        }
+    }
+}
